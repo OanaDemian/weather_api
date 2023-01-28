b655c478511a198e3f2198e8a1da34e12f085237
Oana <oana.demian@gmail.com>
2023-01-28 22:38:07 +0000

    WeatherClient class with fetchWeatherData method

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // file: index.js
-const apiKey = require('./apiKey');
+const apiKey = 'a3d9eb01d4de82b9b8d0849ef604dbed'; // include our key
+const city = 'London';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
 
-// ...
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}
+let weatherData = null;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((weatherData) => {
+    console.log(weatherData);
+    console.log(weatherData.main.temp);
+    console.log(weatherData.weather[0].main);
+  });
+
+console.log('Requesting weather data');
+// const apiKey = require('./apiKey');
