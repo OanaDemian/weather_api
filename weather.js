c0fc099991f58ad82422df3e673f780157d7c836
Oana <oana.demian@gmail.com>
2023-01-29 14:04:05 +0000

    added displayWeather and compareWith methods to the Weather class, compareWith not completed

--- weather.js
+++ weather.js
@@ -13,12 +13,45 @@ class Weather {
   getWeatherData() {
     return this.cityWeatherData;
   }
+
+  compareWith(cityName) {
+
+    const newCity = this.weatherClient.fetchWeatherData(cityName);
+    return newCity.then((weatherData) => {
+      if (
+        this.cityWeatherData[0].main.temp > weatherData.main.temp
+      ) {
+        return `${this.cityWeatherData[0].name} is warmer than ${cityName}.`;
+      } else {
+        return `${cityName} is warmer than ${this.cityWeatherData[0].name} .`;
+      }
+    });
+  }
+
+  displayWeather(cityName) {
+    return this.weatherClient
+      .fetchWeatherData(cityName)
+      .then((weatherData) =>
+        console.log(
+          `City: ${weatherData.name}\n`,
+          `Weather: ${weatherData.weather[0].main}\n`,
+          `Temperature: ${weatherData.main.temp}\n`,
+          `Temperature: ${weatherData.main.temp}\n`,
+          `Feels like: : ${weatherData.main.feels_like}\n`,
+          `Humidity: ${weatherData.main.humidity}\n`
+        )
+      );
+  }
 }
 module.exports = Weather;
 
+// In terminal:
+// const WeatherClient = require('./weatherClient');
 // const weatherClient = new WeatherClient();
+// const Weather = require('./weather');
 // const weather = new Weather(weatherClient);
 // weather.load("Bristol");
-// console.log(weather.getWeatherData());
+// weather.compareWith("London");
+// weather.getWeatherData();
 
 // console.log("this is the last thing");
